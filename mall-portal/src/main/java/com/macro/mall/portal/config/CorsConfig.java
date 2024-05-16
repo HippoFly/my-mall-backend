f05850e4010a7204f5273237d6e4a953bdf9be5c
FlyHippo <whoarewe4u@protonmail.com>
2024-05-16 18:35:11 +0800

    fix(portal): 解决移动端CORS问题

---
+++ mall-portal/src/main/java/com/macro/mall/portal/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.macro.mall.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 全局跨域配置
+ * 注意：前端从网关进行调用时需要配置
+ * Created by macro on 2019/7/27.
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
